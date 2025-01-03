9f7ea05b57b22f945586dd3419d952ac5e7f1eae
prachotx <prachot098@gmail.com>
2025-01-03 10:26:28 +0700

    update ui add address page

--- frontend/src/pages/Address.tsx
+++ frontend/src/pages/Address.tsx
@@ -1,22 +1,53 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import Wrapper from "../components/layout/Wrapper"
 import Navbar from "../components/Navbar"
 import { IoIosArrowForward } from "react-icons/io";
 import axios from "axios";
 import { useNavigate } from "react-router";
+import { getErrorMessage } from "../utils/errorHandler";
+
+// interface Province {
+//     id: number
+//     geography_id: number
+//     name_th: string
+//     name_en: string
+//     created_at: string
+//     updated_at: string
+//     deleted_at: string
+// }
 
 const Address = () => {
-    const [province, setProvince] = useState<string>(""); // จังหวัด
+    const [provinces, setProvinces] = useState<any[]>([]); // จังหวัด
+    const [selectProvince, setSelectProvince] = useState<string>("")
     const [district, setDistrict] = useState<string>(""); // อำเภอ
     const [subDistrict, setSubDistrict] = useState<string>(""); // ตำบล
     const [houseNumber, setHouseNumber] = useState<string>(""); // บ้านเลขที่
     const [village, setVillage] = useState<string>(""); // หมู่
     const [postalCode, setPostalCode] = useState<string>(""); // รหัสไปรษณีย์
+    const [loading, setLoading] = useState<boolean>(false)
+    const [error, setError] = useState<string | null>(null)
     const navigate = useNavigate()
+
+    const fetchProvince = async () => {
+        try {
+            setLoading(true)
+            setError(null)
+            const res = await axios.get("https://raw.githubusercontent.com/kongvut/thai-province-data/master/api_province.json")
+            setProvinces(res.data)
+        } catch (err) {
+            setError(getErrorMessage(err))
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchProvince()
+    }, [])
 
     const handleSubmit = async (event: FormEvent) => {
         event.preventDefault()
-        const address = `จังหวัด ${province} อำเภอ ${district} ตำบล ${subDistrict} บ้านเลขที่ ${houseNumber} หมู่ ${village} รหัสไปรษณีย์ ${postalCode}`
+        const address = `จังหวัด ${selectProvince} อำเภอ ${district} ตำบล ${subDistrict} บ้านเลขที่ ${houseNumber} หมู่ ${village} รหัสไปรษณีย์ ${postalCode}`
         try {
             await axios.post("http://localhost:8080/users/address",
                 { address },
@@ -45,35 +76,40 @@ const Address = () => {
                         </div>
                         <div className="bg-white p-4 rounded">
                             <form onSubmit={handleSubmit}>
-                                <h2>Add Address</h2>
-                                <label>Province:</label>
-                                <select onChange={event => setProvince(event.target.value)}>
-                                    <option value="นครพนม">Province 1</option>
-                                    <option value="2asdasd">Province 2</option>
-                                </select>
-                                <br />
-                                <label>District:</label>
-                                <select onChange={event => setDistrict(event.target.value)}>
-                                    <option value="ธาตุพนท">District 1</option>
-                                    <option value="2asdasd">District 2</option>
-                                </select>
-                                <br />
-                                <label>Subdistrict:</label>
-                                <select onChange={event => setSubDistrict(event.target.value)}>
-                                    <option value="เหนือ">Subdistrict 1</option>
-                                    <option value="2asdasd">Subdistrict 2</option>
-                                </select>
-                                <br />
-                                <label>House Number:</label>
-                                <input type="text" onChange={event => setHouseNumber(event.target.value)} />
-                                <br />
-                                <label>Village:</label>
-                                <input type="text" onChange={event => setVillage(event.target.value)} />
-                                <br />
-                                <label >Postal Code:</label>
-                                <input type="text" onChange={event => setPostalCode(event.target.value)} />
-                                <br />
-                                <button type="submit">Submit</button>
+                                <h2 className="text-xl">Add Address</h2>
+                                <div className="flex flex-col">
+                                    <label>Province</label>
+                                    <select onChange={event => setSelectProvince(event.target.value)} className="border-2 border-gray-300 p-2 rounded">
+                                        {loading ? (
+                                            <option value="2222" selected>loading...</option>
+                                        ) : error ? (
+                                            <option value="2222" selected>{error}</option>
+                                        ) : (
+                                            <option value="2222">1</option>
+                                        )}
+                                    </select>
+                                </div>
+                                <div className="flex flex-col">
+                                    <label>District</label>
+                                    <input type="text" onChange={event => setDistrict(event.target.value)} className="border-2 border-gray-300 p-2 rounded" />
+                                </div>
+                                <div className="flex flex-col">
+                                    <label>Subdistrict</label>
+                                    <input type="text" onChange={event => setSubDistrict(event.target.value)} className="border-2 border-gray-300 p-2 rounded" />
+                                </div>
+                                <div className="flex flex-col">
+                                    <label>House Number:</label>
+                                    <input type="text" onChange={event => setHouseNumber(event.target.value)} className="border-2 border-gray-300 p-2 rounded" />
+                                </div>
+                                <div className="flex flex-col">
+                                    <label>Village:</label>
+                                    <input type="text" onChange={event => setVillage(event.target.value)} className="border-2 border-gray-300 p-2 rounded" />
+                                </div>
+                                <div className="flex flex-col">
+                                    <label >Postal Code:</label>
+                                    <input type="text" onChange={event => setPostalCode(event.target.value)} className="border-2 border-gray-300 p-2 rounded" />
+                                </div>
+                                <button type="submit" className="mt-4 bg-green-400 w-20 h-10 rounded-lg">Submit</button>
                             </form>
                         </div>
                     </div>
